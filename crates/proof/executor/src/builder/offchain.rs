552c42e90f8be830677aeab8a5a4b5d2ad490c4c
Yan Mingzhi <realraindust@gmail.com>
2025-08-22 14:43:37 +0800

    fix: fix get parent info error

--- crates/proof/executor/src/builder/offchain.rs
+++ crates/proof/executor/src/builder/offchain.rs
@@ -67,7 +67,7 @@ where
                 |_| ExecutorError::FraudInitError("Failed to get init accounts code".to_string()),
             )?;
         let parent_info =
-            self.provider.data_by_hash(cal_svm_parent_info(self.parent_slot())).map_err(|_| {
+            self.provider.data_by_hash(cal_svm_parent_info(self.current_slot())).map_err(|_| {
                 ExecutorError::FraudInitError("Failed to get svm parent info code".to_string())
             })?;
         let parent_info: ParentInfo = bincode::deserialize(&parent_info)
